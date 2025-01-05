1f40d8703aa0b30f9db796424da39871d8e8b496
Sithu <cithukyaw@gmail.com>
2025-01-05 16:46:08 +0630

    Add 401/403 check to redirect to login

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import {ToastContainer} from "react-toastify";
-import {Outlet} from "react-router-dom";
-import {getItemDecrypted} from "../helpers/storage";
+import {Outlet, useNavigate} from "react-router-dom";
+import {getItemDecrypted, removeItem} from "../helpers/storage";
 import config from "../config";
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
@@ -10,13 +10,20 @@ import {fetchSettings} from "../state/setting/settingSlice";
 const Layout = () => {
     const user = getItemDecrypted(config.userStoreKey);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     useEffect(() => {
         dispatch(fetchSettings());
         if (user) {
-            dispatch(userAccount(user.account_id));
+            dispatch(userAccount(user.account_id)).then(response => {
+                const { error } = response.payload;
+                if (error && (error.status === 401 || error.status === 403)) {
+                    removeItem(config.userStoreKey);
+                    navigate("/login");
+                }
+            });
         }
-    }, []);
+    }, [user, navigate]);
 
     return (
         <div className="app">
